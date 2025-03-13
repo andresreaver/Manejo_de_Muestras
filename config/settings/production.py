113d53018a00fa390ea99fa4d92ba54f09b2fbe2
Andresreaver <andresreaver@gmail.com>
2025-03-13 14:23:03 -0500

    Prueba de conexión sin variables de entorno

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ["10.26.3.248"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("blkpyapp", "nombre_de_tu_bd"),
-        "USER": os.environ.get("db_admin", "usuario_bd"),
-        "PASSWORD": os.environ.get("PSQL_INSBlk_2024", "contraseña_bd"),
-        "HOST": os.environ.get("DB_HOST", "10.26.3.248"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+        "NAME": "blkpyapp",
+        "USER": "db_admin",
+        "PASSWORD": "PSQL_INSBlk.2024",
+        "HOST": "10.26.3.248",
+        "PORT": "5432",
     }
 }
