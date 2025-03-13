2bd46b020a3d782c9e29a44c10a68f8f29fcde35
Andresreaver <andresreaver@gmail.com>
2025-03-13 14:07:28 -0500

    actualizacion de datos de servidor

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ["10.26.3.248"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("blkpyapp"),
-        "USER": os.getenv("db_admin"),
-        "PASSWORD": os.getenv("PSQL_INSBlk.2024"),
-        "HOST": os.getenv("10.26.3.248"),
-        "PORT": os.getenv("5432"),
+        "NAME": os.environ.get("blkpyapp", "nombre_de_tu_bd"),
+        "USER": os.environ.get("db_admin", "usuario_bd"),
+        "PASSWORD": os.environ.get("PSQL_INSBlk_2024", "contraseña_bd"),
+        "HOST": os.environ.get("DB_HOST", "10.26.3.248"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
