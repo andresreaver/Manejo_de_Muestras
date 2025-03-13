b10fe04c090a65a5e600f54339ee8eba6e0250db
Andresreaver <andresreaver@gmail.com>
2025-03-13 15:03:20 -0500

    Volviendo a variables de entorno para conexión segura

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ["10.26.3.248"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "blkpyapp",
-        "USER": "db_admin",
-        "PASSWORD": "PSQL-INSBlk.2024",
-        "HOST": "10.26.3.248",
-        "PORT": "5432",
+        "NAME": os.environ.get("BLKPYAPP_DB_NAME"),
+        "USER": os.environ.get("BLKPYAPP_DB_USER"),
+        "PASSWORD": os.environ.get("BLKPYAPP_DB_PASSWORD"),
+        "HOST": os.environ.get("BLKPYAPP_DB_HOST", "10.26.3.248"),
+        "PORT": os.environ.get("BLKPYAPP_DB_PORT", "5432"),
     }
 }
