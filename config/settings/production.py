fd118b2b57b9a4be736b9666b0f6d72a6f17548b
Andresreaver <andresreaver@gmail.com>
2025-03-17 10:02:04 -0500

    cambio en base de datos para produccion

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,11 +6,11 @@ ALLOWED_HOSTS = ["10.26.3.248"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("BLKPYAPP_DB_NAME"),
-        "USER": os.environ.get("BLKPYAPP_DB_USER"),
-        "PASSWORD": os.environ.get("BLKPYAPP_DB_PASSWORD"),
-        "HOST": os.environ.get("BLKPYAPP_DB_HOST", "10.26.3.248"),
-        "PORT": os.environ.get("BLKPYAPP_DB_PORT", "5432"),
+        "NAME": "blkpyapp",
+        "USER": "postgres",
+        "PASSWORD": "Admin1234",
+        "HOST": "10.26.3.248",
+        "PORT": "5432",
     }
 }
 CSRF_TRUSTED_ORIGINS = ["http://10.26.3.248"]
