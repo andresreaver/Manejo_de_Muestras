1d50099f64a5a35d58679ac7fa395670372ec764
Andresreaver <andresreaver@gmail.com>
2025-03-25 14:09:51 -0500

    cambio en la configuracion de produccion

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,9 +20,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-CSRF_TRUSTED_ORIGINS = ["http://10.26.3.248"
-                        "https://balalaika.app"
-                    ]
+CSRF_TRUSTED_ORIGINS = [
+        "http://10.26.3.248",
+        "https://balalaika.app",
+    ]
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -42,6 +43,7 @@ LOGGING = {
     },
 }
 
+STATICFILES_DIRS = [BASE_DIR / "config/static"]
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "config/staticfiles"
 
